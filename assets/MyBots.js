cfeb8f3d80cae9f392ad45d4132345e2dff9d7e1
Daniel <artsjedi@hotmail.com>
2014-03-10 21:03:54 -0300

    Added image assests

---
+++ assets/MyBots.js
@@ -0,0 +1,418 @@
+(function (lib, img, cjs) {
+
+var p; // shortcut to reference prototypes
+
+// stage content:
+(lib.MyBots = function() {
+	this.initialize();
+
+	// Camada 15
+	this.Bot09 = new lib.Bot09_1();
+	this.Bot09.setTransform(192.9,1781.7,1,1,0,0,0,180.7,236.6);
+
+	// Camada 17
+	this.Bot13 = new lib.Bot13_1();
+	this.Bot13.setTransform(1412.4,1791.7,1,1,0,0,0,204.1,213.2);
+
+	// Camada 16
+	this.Bot11 = new lib.Bot11_1();
+	this.Bot11.setTransform(1098.1,1789.9,1,1,0,0,0,100.5,197);
+
+	this.Bot12 = new lib.Bot12_1();
+	this.Bot12.setTransform(953.8,1690.8,1,1,0,0,0,100.5,197);
+
+	this.Bot10 = new lib.Bot10_1();
+	this.Bot10.setTransform(724.1,1797.9,1,1,0,0,0,100.5,197);
+
+	// Camada 14
+	this.Bot08 = new lib.Bot08_1();
+	this.Bot08.setTransform(518.2,1628.4,1,1,0,0,0,170.1,175.8);
+
+	// Camada 13
+	this.Bot01 = new lib.Bot01_1();
+	this.Bot01.setTransform(816.6,1438.3,1,1,0,0,0,248.4,190.8);
+
+	// Camada 12
+	this.Bot03 = new lib.Bot03_1();
+	this.Bot03.setTransform(1312.3,1416.3,1,1,0,0,0,249.9,228);
+
+	// Camada 11
+	this.Bot02 = new lib.Bot02_1();
+	this.Bot02.setTransform(258.1,1382.4,1,1,0,0,0,276,188.1);
+
+	// Camada 10
+	this.Bot04 = new lib.Bot04_1();
+	this.Bot04.setTransform(1216.8,1210,1,1,0,0,0,199.3,158.8);
+
+	// Camada 9
+	this.Bot07 = new lib.Bot07_1();
+	this.Bot07.setTransform(1276.4,877,1,1,0,0,0,153.9,147.3);
+
+	// Camada 8
+	this.Bot05 = new lib.Bot05_1();
+	this.Bot05.setTransform(843.7,895.8,1,1,0,0,0,116.3,151.5);
+
+	// Camada 7
+	this.Bot06 = new lib.Bot06_1();
+	this.Bot06.setTransform(596.2,888.3,1,1,0,0,0,115.3,152);
+
+	// Camada 6
+	this.Bot16 = new lib.Bot16_1();
+	this.Bot16.setTransform(276.1,376.2,1,1,0,0,0,242,242);
+
+	// Camada 4
+	this.Bot14 = new lib.Bot14_1();
+	this.Bot14.setTransform(242.1,1080.3,1,1,0,0,0,317.1,310.1);
+
+	// Camada 5
+	this.Bot15 = new lib.Bot15_1();
+	this.Bot15.setTransform(204,632.2,1,1,0,0,0,190.5,194);
+
+	// Camada 2
+	this.Bot17 = new lib.Bot17_1();
+	this.Bot17.setTransform(1412.4,732.2,1,1,0,0,0,618.5,584.5);
+
+	// Camada 3
+	this.Bot18 = new lib.Bot18_1();
+	this.Bot18.setTransform(694.7,452.1,1,1,0,0,0,213.8,235);
+
+	// Camada 1
+	this.instance = new lib.mybotsbg();
+
+	this.addChild(this.instance,this.Bot18,this.Bot17,this.Bot15,this.Bot14,this.Bot16,this.Bot06,this.Bot05,this.Bot07,this.Bot04,this.Bot02,this.Bot03,this.Bot01,this.Bot08,this.Bot10,this.Bot12,this.Bot11,this.Bot13,this.Bot09);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(-75,0,2105.9,2048);
+
+
+// symbols:
+(lib.Bot01 = function() {
+	this.initialize(img.Bot01);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,828,636);
+
+
+(lib.Bot02 = function() {
+	this.initialize(img.Bot02);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,920,627);
+
+
+(lib.Bot03 = function() {
+	this.initialize(img.Bot03);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,833,760);
+
+
+(lib.Bot04 = function() {
+	this.initialize(img.Bot04);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,797,635);
+
+
+(lib.Bot05 = function() {
+	this.initialize(img.Bot05);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,465,606);
+
+
+(lib.Bot06 = function() {
+	this.initialize(img.Bot06);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,461,608);
+
+
+(lib.Bot07 = function() {
+	this.initialize(img.Bot07);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,513,491);
+
+
+(lib.Bot08 = function() {
+	this.initialize(img.Bot08);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,567,586);
+
+
+(lib.Bot09 = function() {
+	this.initialize(img.Bot09);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,556,728);
+
+
+(lib.Bot10 = function() {
+	this.initialize(img.Bot10);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,402,788);
+
+
+(lib.Bot11 = function() {
+	this.initialize(img.Bot11);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,402,788);
+
+
+(lib.Bot12 = function() {
+	this.initialize(img.Bot12);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,402,788);
+
+
+(lib.Bot13 = function() {
+	this.initialize(img.Bot13);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,583,609);
+
+
+(lib.Bot14 = function() {
+	this.initialize(img.Bot14);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,906,886);
+
+
+(lib.Bot15 = function() {
+	this.initialize(img.Bot15);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,762,776);
+
+
+(lib.Bot16 = function() {
+	this.initialize(img.Bot16);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,801,568);
+
+
+(lib.Bot17 = function() {
+	this.initialize(img.Bot17);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1767,1670);
+
+
+(lib.Bot18 = function() {
+	this.initialize(img.Bot18);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,855,940);
+
+
+(lib.mybotsbg = function() {
+	this.initialize(img.mybotsbg);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,1536,2048);
+
+
+(lib.Bot18_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot18();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,427.5,470);
+
+
+(lib.Bot17_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot17();
+	this.instance.setTransform(0,0,0.7,0.7);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,1236.9,1169);
+
+
+(lib.Bot16_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot16();
+	this.instance.setTransform(0,283.2,0.5,0.5,-44.9);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,484,484);
+
+
+(lib.Bot15_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot15();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,381,388);
+
+
+(lib.Bot14_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot14();
+	this.instance.setTransform(0,0,0.7,0.7);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,634.2,620.2);
+
+
+(lib.Bot13_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot13();
+	this.instance.setTransform(0,0,0.7,0.7);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,408.1,426.3);
+
+
+(lib.Bot12_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot12();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,201,394);
+
+
+(lib.Bot11_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot11();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,201,394);
+
+
+(lib.Bot10_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot10();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,201,394);
+
+
+(lib.Bot09_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot09();
+	this.instance.setTransform(0,0,0.65,0.65);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,361.4,473.2);
+
+
+(lib.Bot08_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot08();
+	this.instance.setTransform(0,0,0.6,0.6);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,340.2,351.6);
+
+
+(lib.Bot07_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot07();
+	this.instance.setTransform(0,0,0.6,0.6);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,307.8,294.6);
+
+
+(lib.Bot06_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot06();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,230.5,304);
+
+
+(lib.Bot05_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot05();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,232.5,303);
+
+
+(lib.Bot04_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot04();
+	this.instance.setTransform(0,0,0.5,0.5);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,398.5,317.5);
+
+
+(lib.Bot03_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot03();
+	this.instance.setTransform(0,0,0.6,0.6);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,499.8,456);
+
+
+(lib.Bot02_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot02();
+	this.instance.setTransform(0,0,0.6,0.6);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,552,376.2);
+
+
+(lib.Bot01_1 = function() {
+	this.initialize();
+
+	// Layer 1
+	this.instance = new lib.Bot01();
+	this.instance.setTransform(0,0,0.6,0.6);
+
+	this.addChild(this.instance);
+}).prototype = p = new cjs.Container();
+p.nominalBounds = new cjs.Rectangle(0,0,496.8,381.6);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{});
+var lib, images, createjs;
