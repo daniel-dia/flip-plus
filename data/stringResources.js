631fd8a355d6d8fc8e880d9aba39dea13991ca3c
artsjedi@hotmail.com <artsjedi@hotmail.com>
2014-06-04 18:03:04 -0300

    String em outra pasta

---
+++ data/stringResources.js
@@ -0,0 +1,43 @@
+﻿var stringResources=//: InvertCross.StringResources =
+{
+    ld: "Loading",
+
+    it_text1: "N3-S needs \n repair",
+    it_text2: "alone = bad\nfriends=good",
+
+    mm_play: "PLAY",
+
+    op_back: "Back",
+    op_erase: "Erase All Data",
+    op_options: "Options",
+
+    pr_notStarsTitle: "Not enught stars",
+    pr_notStarsText: "you only have # stars. \nYou need at least stars # \nto unlock this project\n play more levels to earn stars.",
+    pr_notTimeText: "Not Yet.#You must wait # before play this bonus level",
+
+    ws_Locked: "LOCKED",
+    ws_NotFree: "NOT FREE",
+
+    gp_noMoreSkip: "no more skips",
+    gp_noMoreHints: "no more hints",
+    gp_finishPuzzle: "Well done",
+
+    gp_pz_Popup1Title: "Time Attack",
+    gp_pz_Popup1Text1: "Solve",
+    gp_pz_Popup1Text2: "boards in",
+    gp_pz_Popup1Text3: "seconds",
+    gp_pz_statusEnd: "END",
+    gp_pz_timeUP: "Time's up",
+
+    gp_mv_Popup1Title: "Flip Challenge",
+    gp_mv_Popup1Text1: "Solve",
+    gp_mv_Popup1Text2: "boards in",
+    gp_mv_Popup1Text3: "flips",
+    gp_mv_statusEnd: "END",
+    gp_mv_noMoreMoves: "No more moves",
+
+    b1_popup1Ttitle: "Pick 3 Barrels",
+    b1_popup1Text: "Some Barrels has hiddens items",
+    b2_noMoreChances: "No more chances",
+    b2_finish: "Well done!",
+}
