e2aca165d31a825cc68c8cf233f708c4030c73c2
artsjedi@hotmail.com <artsjedi@hotmail.com>
2014-07-27 13:19:04 -0300

    Assets

--- assets/resize.bat
+++ assets/resize.bat
@@ -2,10 +2,9 @@ xcopy images_1x images_0.5x /y /s
 cd images_0.5x
 for /r %%f in (*.*) do convert %%f -resize 50%% %%f
 cd..
-
-xcopy images_1x images_0.25x /y /s
+xcopy images_0.5x images_0.25x /y /s
 cd images_0.25x
-for /r %%f in (*.*) do convert %%f -resize 25%% %%f
+for /r %%f in (*.*) do convert %%f -resize 50%% %%f
 cd..
 
 @pause
