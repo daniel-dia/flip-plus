6f600f5739a4a9f134a607f73c39d19c862be358
Daniel Santos <artsjedi@hotmail.com>
2014-11-21 23:07:04 -0200

    Changed from money to coins

--- src/UserData/Coins.ts
+++ src/UserData/Coins.ts
@@ -1,31 +1,31 @@
 ﻿module FlipPlus.UserData {
 
     // Class
-    export class Money {
+    export class Coins {
         
-        // amount of money
+        // coins amount
         private ammount: number;
 
         // class constructor
         constructor() {
-            var data = localStorage.getItem(storagePrefix + "Money");
+            var data = localStorage.getItem(storagePrefix + "Coins");
             if (data)
                 this.ammount = JSON.parse(data);
             else this.ammount = 0;
         }
 
-        // get amount of money
+        // get coins amount
         public getAmount(): number {
            return this.ammount ? this.ammount : 0;
         }
 
-        // set amount of money
+        // set coins amount
         public setAmount(value: number) {
             this.ammount = value;
-            localStorage.setItem(storagePrefix + "Money", JSON.stringify(Money));
+            localStorage.setItem(storagePrefix + "Coins", JSON.stringify(Coins));
         }
 
-        // increase money ammount
+        // increase coins ammount
         public increaseAmount(value: number= 1) {
 
             // doesen't accept zero or negative values
@@ -33,16 +33,16 @@
 
             var iq: number = this.getAmount();
             
-            // limit money quantity to 10
-            // TODO : verify if money must be less than 10
+            // limit coins quantity to 10
+            // TODO : verify if coins must be less than 10
             var total = iq + value;
             if (total >= 10) total = 10;
 
-            // set money ammount            
+            // set coins ammount            
             this.setAmount(total);
         }
 
-        // decrease money value
+        // decrease coins value
         public decreaseAmount(value: number= 1) {
             
             // doesen't accept zero or negative values
