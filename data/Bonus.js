eee57121f509ecfd0d6cc2c73af2a1e269b7bd09
dfsantos <dfsantos@odebrecht.com>
2016-05-06 09:24:49 -0300

    change bonus time

--- data/Bonus.js
+++ data/Bonus.js
@@ -1,6 +1,6 @@
 ﻿//time in minutes
 var bonusData = {
-    "Bonus1": { id: "Bonus1", cost: 6, timeOut:  12  },
-    "Bonus2": { id: "Bonus2", cost: 20, timeOut: 18  },
-    "Bonus3": { id: "Bonus3", cost: 30, timeOut: 24  }
+    "Bonus1": { id: "Bonus1", cost: 6, timeOut:  12*30  },
+    "Bonus2": { id: "Bonus2", cost: 20, timeOut: 18*30  },
+    "Bonus3": { id: "Bonus3", cost: 30, timeOut: 24*30  }
 }
