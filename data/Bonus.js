031934f90975855927e448eb10b0fbe0f600f4bd
Daniel Santos <artsjedi@hotmail.com>
2016-06-04 20:16:11 -0300

    change bonus time for using

--- data/Bonus.js
+++ data/Bonus.js
@@ -1,6 +1,6 @@
 ﻿//time in minutes
 var bonusData = {
-    "Bonus1": { unlock:1, id: "Bonus1", cost: 6, timeOut:  12*30  },
-    "Bonus2": { unlock:2, id: "Bonus2", cost: 20, timeOut: 18*30  },
-    "Bonus3": { unlock:3, id: "Bonus3", cost: 30, timeOut: 24*30  }
+    "Bonus1": { unlock:1, id: "Bonus1", cost: 6, timeOut:  2*60  },
+    "Bonus2": { unlock:2, id: "Bonus2", cost: 20, timeOut: 3*60  },
+    "Bonus3": { unlock:3, id: "Bonus3", cost: 30, timeOut: 4*30  }
 }
